ccb54d9851ceafcd080e512cc25e3d869d302125
kedarnath tripathi <tripathikd786@gmail.com>
2026-08-02 10:19:16 +0530

    Use Node 20

--- config/mail.js
+++ config/mail.js
@@ -1,14 +1,23 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  family: 4, // Force IPv4
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});
+  tls: {
+    rejectUnauthorized: false,
+  },
+  getSocket: (options, callback) => {
+    dns.lookup(options.host, { family: 4 }, (err, address) => {
+      if (err) return callback(err);
 
-module.exports = transporter;
+      options.host = address;
+      callback(null, false);
+    });
+  },
+});
