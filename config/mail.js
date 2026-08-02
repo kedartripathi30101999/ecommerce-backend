b7316ba552e0e66561fcc600b2539403d260e390
kedarnath tripathi <tripathikd786@gmail.com>
2026-08-02 10:51:29 +0530

    last changes

--- config/mail.js
+++ config/mail.js
@@ -1,24 +1,10 @@
 const nodemailer = require("nodemailer");
-const dns = require("dns");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-  getSocket: (options, callback) => {
-    dns.lookup(options.host, { family: 4 }, (err, address) => {
-      if (err) return callback(err);
-
-      options.host = address;
-      callback(null, false);
-    });
   },
 });
 
