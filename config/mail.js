218e912eeb6aacbeda8d5ba4af649eb93d697446
kedarnath tripathi <tripathikd786@gmail.com>
2026-08-02 09:42:18 +0530

    Check mail transporter

--- config/mail.js
+++ config/mail.js
@@ -1,11 +1,19 @@
-const nodemailer= require('nodemailer')
+const nodemailer = require("nodemailer");
 
-const transporter= nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
-})
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
-module.exports= transporter
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("Mail Error:", error);
+  } else {
+    console.log("Mail Server Ready");
+  }
+});
+
+module.exports = transporter;
