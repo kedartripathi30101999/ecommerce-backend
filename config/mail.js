38096b888903a8ed9848888800b73f5cf883568f
kedarnath tripathi <tripathikd786@gmail.com>
2026-08-02 10:11:00 +0530

    Force IPv4 for Gmail SMTP

--- config/mail.js
+++ config/mail.js
@@ -4,12 +4,11 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
+  family: 4, // Force IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  logger: true,
-  debug: true,
 });
 
 module.exports = transporter;
