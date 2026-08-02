4e2b3a41c40851e7621dbdfec975393cfdf49954
kedarnath tripathi <tripathikd786@gmail.com>
2026-08-02 09:48:34 +0530

    Update SMTP configuration

--- config/mail.js
+++ config/mail.js
@@ -1,19 +1,16 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});
-
-transporter.verify((error, success) => {
-  if (error) {
-    console.log("Mail Error:", error);
-  } else {
-    console.log("Mail Server Ready");
-  }
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 module.exports = transporter;
